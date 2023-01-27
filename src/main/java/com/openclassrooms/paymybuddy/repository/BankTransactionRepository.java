4be2aadb8d37a023ddede3ec6899f01b2ceac380
4H0 <ggaetan@proton.me>
2023-01-27 09:10:26 +0100

    Create repository interfaces

---
+++ src/main/java/com/openclassrooms/paymybuddy/repository/BankTransactionRepository.java
@@ -0,0 +1,9 @@
+package com.openclassrooms.paymybuddy.repository;
+
+import com.openclassrooms.paymybuddy.model.BankTransaction;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankTransactionRepository extends CrudRepository<BankTransaction, Integer> {
+}
