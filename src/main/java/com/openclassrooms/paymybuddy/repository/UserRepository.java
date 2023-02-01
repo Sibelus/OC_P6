112679f197527e667dbe5f7efe8e56cd19b59194
4H0 <ggaetan@proton.me>
2023-02-01 09:11:30 +0100

    Add findByEmail() method

--- src/main/java/com/openclassrooms/paymybuddy/repository/UserRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.openclassrooms.paymybuddy.repository;
 
 import com.openclassrooms.paymybuddy.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
 }
