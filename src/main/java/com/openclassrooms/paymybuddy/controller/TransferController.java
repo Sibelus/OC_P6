6f88582fdf25cfe1139203c76a159ee177441771
4H0 <ggaetan@proton.me>
2023-02-03 09:41:28 +0100

    Update Transfer controller & create html page

--- src/main/java/com/openclassrooms/paymybuddy/controller/TransferController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/TransferController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -18,8 +22,17 @@ public class TransferController {
     @GetMapping("/transfer")
     public String welcomePage(Model model, String email) {
         Optional<User> newFriend = iUserService.findByEmail(email);
+        List<User> friendList = new ArrayList<>();
 
         model.addAttribute("newFriend", newFriend);
+        model.addAttribute("friendList", friendList);
+        return "transfer";
+    }
+
+    @PostMapping("/transfer_newFriend")
+    public String createAccountSubmit(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", user);
+        iUserService.addUser(user);
         return "transfer";
     }
 }
