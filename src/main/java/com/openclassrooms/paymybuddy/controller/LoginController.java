311dceaffac72039e2794437fcb6eeb4ca26a89f
4H0 <ggaetan@proton.me>
2023-02-03 09:38:54 +0100

    Add createAccount mapping to controller & create html page

--- src/main/java/com/openclassrooms/paymybuddy/controller/LoginController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/LoginController.java
@@ -1,15 +1,44 @@
 package com.openclassrooms.paymybuddy.controller;
 
-import jakarta.annotation.security.RolesAllowed;
+import com.openclassrooms.paymybuddy.model.User;
+import com.openclassrooms.paymybuddy.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class LoginController {
 
-    @GetMapping("/*")
-    @RolesAllowed("USER")
-    public String getUser(){
-        return "Welcome user ^^";
+    @Autowired
+    private IUserService iUserService;
+
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        return "login";
     }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @GetMapping("/createAccount")
+    public String createAccountForm(Model model) {
+        model.addAttribute("user", new User());
+        return "createAccount";
+    }
+
+    @PostMapping("/createAccount")
+    public String createAccountSubmit(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", user);
+        iUserService.addUser(user);
+        return "login";
+    }
+
+
 }
