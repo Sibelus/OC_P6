08f4b5c22c692e165c17038533a6bee2ce7d13f6
4H0 <ggaetan@proton.me>
2023-02-07 15:19:17 +0100

    Create findByUserId() method & extends JpaRepository

--- src/main/java/com/openclassrooms/paymybuddy/repository/ConnectionRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/ConnectionRepository.java
@@ -1,9 +1,12 @@
 package com.openclassrooms.paymybuddy.repository;
 
 import com.openclassrooms.paymybuddy.model.Connection;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ConnectionRepository extends CrudRepository<Connection, Integer> {
+public interface ConnectionRepository extends JpaRepository<Connection, Integer> {
+    List<Connection> findByUserId(int userId);
 }
