d976e44260a2920ac05313536a3eed9ce698d4d6
4H0 <ggaetan@proton.me>
2023-02-08 15:27:05 +0100

    Update to JpaRepository

--- src/main/java/com/openclassrooms/paymybuddy/repository/BankTransactionRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/BankTransactionRepository.java
@@ -1,9 +1,9 @@
 package com.openclassrooms.paymybuddy.repository;
 
 import com.openclassrooms.paymybuddy.model.BankTransaction;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BankTransactionRepository extends CrudRepository<BankTransaction, Integer> {
+public interface BankTransactionRepository extends JpaRepository<BankTransaction, Integer> {
 }
