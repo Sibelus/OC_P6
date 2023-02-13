0947020eb764af06fbc0ccc00133fef70a6513be
4H0 <ggaetan@proton.me>
2023-02-13 11:04:17 +0100

    Refactor InAppTransactionRepository to extends JpaRepository

--- src/main/java/com/openclassrooms/paymybuddy/repository/InAppTransactionRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/InAppTransactionRepository.java
@@ -1,9 +1,9 @@
 package com.openclassrooms.paymybuddy.repository;
 
 import com.openclassrooms.paymybuddy.model.InAppTransaction;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InAppTransactionRepository extends CrudRepository<InAppTransaction, Integer> {
+public interface InAppTransactionRepository extends JpaRepository<InAppTransaction, Integer> {
 }
