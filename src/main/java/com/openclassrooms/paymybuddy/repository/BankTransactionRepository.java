a20d1421acee2d462fb7fd387210f9c5b163079e
4H0 <ggaetan@proton.me>
2023-02-13 12:40:36 +0100

    Create findByUserId() method

--- src/main/java/com/openclassrooms/paymybuddy/repository/BankTransactionRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/BankTransactionRepository.java
@@ -4,6 +4,9 @@ import com.openclassrooms.paymybuddy.model.BankTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BankTransactionRepository extends JpaRepository<BankTransaction, Integer> {
+    List<BankTransaction> findByUserId(int id);
 }
