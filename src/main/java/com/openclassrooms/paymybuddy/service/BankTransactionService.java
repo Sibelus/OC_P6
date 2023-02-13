a87f45a8cf1cc3128fece39ab76fbd4077c365fb
4H0 <ggaetan@proton.me>
2023-02-13 12:41:14 +0100

    Refactor BankTransactionService classes

--- src/main/java/com/openclassrooms/paymybuddy/service/BankTransactionService.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/BankTransactionService.java
@@ -7,6 +7,8 @@ import com.openclassrooms.paymybuddy.repository.BankTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BankTransactionService implements IBankTransationService{
 
@@ -20,21 +22,37 @@ public class BankTransactionService implements IBankTransationService{
 
     @Override
     public void bankToUser(BankTransaction bankTransaction) {
+        //Update user inApp credit
         User currentUser = bankTransaction.getUser();
         currentUser.setAmount(currentUser.getAmount() + bankTransaction.getAmount());
+        //Update bank account credit
         BankAccount bankAccount = bankTransaction.getBankAccount();
         bankAccount.setAmount(bankAccount.getAmount() - bankTransaction.getAmount());
+        //Calculate fee for the transaction
+        bankTransaction.setFee(iFeeService.calculateFee(bankTransaction.getAmount()));
 
         bankTransactionRepository.save(bankTransaction);
     }
 
     @Override
     public void userToBank(BankTransaction bankTransaction) {
+        //Update user inApp credit
         User currentUser = bankTransaction.getUser();
         currentUser.setAmount(currentUser.getAmount() - bankTransaction.getAmount());
+        //Update bank account credit
         BankAccount bankAccount = bankTransaction.getBankAccount();
         bankAccount.setAmount(bankAccount.getAmount() + bankTransaction.getAmount());
+        //Calculate fee for the transaction
+        bankTransaction.setFee(iFeeService.calculateFee(bankTransaction.getAmount()));
+        //Set amount of transaction to negative value -> help visualize exiting money from application
+        bankTransaction.setAmount(bankTransaction.getAmount()*(-1));
 
         bankTransactionRepository.save(bankTransaction);
+    }
+
+    @Override
+    public List<BankTransaction> getBankTransactions(int userId) {
+        List<BankTransaction> bankTransactions = bankTransactionRepository.findByUserId(userId);
+        return bankTransactions;
     }
 }
--- src/main/java/com/openclassrooms/paymybuddy/service/IBankTransationService.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/IBankTransationService.java
@@ -1,12 +1,14 @@
 package com.openclassrooms.paymybuddy.service;
 
-import com.openclassrooms.paymybuddy.model.BankAccount;
 import com.openclassrooms.paymybuddy.model.BankTransaction;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface IBankTransationService {
 
     void bankToUser(BankTransaction bankTransaction);
     void userToBank(BankTransaction bankTransaction);
+    List<BankTransaction> getBankTransactions(int userId);
 }
