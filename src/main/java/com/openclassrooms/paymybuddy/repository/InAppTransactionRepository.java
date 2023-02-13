0d0fead39004bf2889bb82ea9455b055ebd2f0d5
4H0 <ggaetan@proton.me>
2023-02-13 12:42:15 +0100

    Create findBySenderId() method

--- src/main/java/com/openclassrooms/paymybuddy/repository/InAppTransactionRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/InAppTransactionRepository.java
@@ -4,6 +4,9 @@ import com.openclassrooms.paymybuddy.model.InAppTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InAppTransactionRepository extends JpaRepository<InAppTransaction, Integer> {
+    List<InAppTransaction> findBySenderId(int id);
 }
