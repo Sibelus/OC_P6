87e04bc71b4c4c1231ea839dd869f3385db0f9e0
4H0 <ggaetan@proton.me>
2023-02-14 10:15:44 +0100

    Add some tests

--- src/main/java/com/openclassrooms/paymybuddy/PaymybuddyApplication.java
+++ src/main/java/com/openclassrooms/paymybuddy/PaymybuddyApplication.java
@@ -1,9 +1,8 @@
 package com.openclassrooms.paymybuddy;
 
-import com.openclassrooms.paymybuddy.model.BankAccount;
-import com.openclassrooms.paymybuddy.model.Connection;
-import com.openclassrooms.paymybuddy.model.User;
+import com.openclassrooms.paymybuddy.model.*;
 import com.openclassrooms.paymybuddy.service.IBankAccountService;
+import com.openclassrooms.paymybuddy.service.IBankTransationService;
 import com.openclassrooms.paymybuddy.service.IConnectionService;
 import com.openclassrooms.paymybuddy.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +20,10 @@ public class PaymybuddyApplication implements CommandLineRunner {
 	private IUserService iUserService;
 	@Autowired
 	private IBankAccountService iBankAccountService;
-
 	@Autowired
 	private IConnectionService iConnectionService;
+	@Autowired
+	private IBankTransationService iBankTransationService;
 
 
 	public static void main(String[] args) {
@@ -36,11 +36,15 @@ public class PaymybuddyApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Optional<User> user1 = iUserService.findByEmail("mtampion@mail.fr");
 		System.out.println(user1.get().getFirstname());
+		System.out.println(user1.get().getFriendsList());
+		System.out.println(user1.get().getBankTransactions());
+		System.out.println(user1.get().getInAppTransactions());
 
-
-		//iConnectionService.addFriendship(1,3);
 		List<Connection> friendshipList = iConnectionService.getFriendshipList(1);
 		friendshipList.forEach(connection -> System.out.println(connection.getFriend().getFirstname()));
+
+		List<BankTransaction> bankTransactions = iBankTransationService.getBankTransactions(1);
+		System.out.println(bankTransactions);
 
 		user1 = iUserService.findByEmail("mtampion@mail.fr");
 		System.out.println(user1.get().getFriendsList());
