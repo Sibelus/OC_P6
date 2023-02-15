00e252755952004d27d76993eefb4a3e6420af15
4H0 <ggaetan@proton.me>
2023-02-15 15:47:46 +0100

    Add logging

--- src/main/java/com/openclassrooms/paymybuddy/service/FeeService.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/FeeService.java
@@ -1,12 +1,18 @@
 package com.openclassrooms.paymybuddy.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FeeService implements IFeeService {
+
+    Logger logger = LoggerFactory.getLogger(FeeService.class);
+
     @Override
     public Float calculateFee(int amount) {
         Float fee = (float)(amount*5)/100;
+        logger.debug("For {}, fees are equals to {}", amount, fee);
         return fee;
     }
 }
