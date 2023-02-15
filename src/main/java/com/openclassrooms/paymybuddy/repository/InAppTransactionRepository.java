b49aa9c9020920e8c08157586e5ae041fc6354b1
4H0 <ggaetan@proton.me>
2023-02-15 15:24:14 +0100

    Remove useless import

--- src/main/java/com/openclassrooms/paymybuddy/repository/InAppTransactionRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/InAppTransactionRepository.java
@@ -4,8 +4,6 @@ import com.openclassrooms.paymybuddy.model.InAppTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface InAppTransactionRepository extends JpaRepository<InAppTransaction, Integer> {
 }
