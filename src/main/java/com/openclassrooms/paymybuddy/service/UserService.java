119897b5916c287464d0933df6e29eaf775c8f21
4H0 <ggaetan@proton.me>
2023-02-16 17:43:47 +0100

    Add update & delete methods and test EntityManager not achieved

--- src/main/java/com/openclassrooms/paymybuddy/service/UserService.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/UserService.java
@@ -6,10 +6,14 @@ import com.openclassrooms.paymybuddy.service.exceptions.EmptyEmailException;
 import com.openclassrooms.paymybuddy.service.exceptions.EmptyFirstnameException;
 import com.openclassrooms.paymybuddy.service.exceptions.EmptyLastnameException;
 import com.openclassrooms.paymybuddy.service.exceptions.EmptyPasswordException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,14 +21,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @Service
 public class UserService implements IUserService{
 
     @Autowired
     private UserRepository userRepository;
+    private EntityManager entityManager;
     Logger logger  = LoggerFactory.getLogger(UserService.class);
 
+
+    public UserService(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public Optional<User> findByEmail(String email) {
@@ -32,7 +42,7 @@ public class UserService implements IUserService{
     }
 
     @Override
-    public User addUser(User user) throws EmptyEmailException, EmptyFirstnameException, EmptyPasswordException, EmptyLastnameException {
+    public void addUser(User user) throws EmptyEmailException, EmptyFirstnameException, EmptyPasswordException, EmptyLastnameException {
         if(user.getFirstname().equals("")){
             logger.error("Firstname provided is empty");
             throw new EmptyFirstnameException("Firstname provided is empty, you must set a valid one");
@@ -54,7 +64,46 @@ public class UserService implements IUserService{
         String password = passwordEncoder.encode(user.getPassword());
         user.setPassword(password);
         logger.debug("Save new user");
-        return userRepository.save(user);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user) throws EmptyFirstnameException, EmptyLastnameException, EmptyEmailException, EmptyPasswordException {
+        if(user.getFirstname().equals("")){
+            logger.error("Firstname provided is empty");
+            throw new EmptyFirstnameException("Firstname provided is empty, you must set a valid one");
+        }
+        if(user.getLastname().equals("")){
+            logger.error("Firstname provided is empty");
+            throw new EmptyLastnameException("Lastname provided is empty, you must set a valid one");
+        }
+        if(user.getEmail().equals("")){
+            logger.error("Email provided is empty");
+            throw new EmptyEmailException("Email provided is empty, you must set a valid one");
+        }
+        if(user.getPassword().equals("")){
+            logger.error("Password provided is empty");
+            throw new EmptyPasswordException("Password provided is empty, you must set a valid one");
+        }
+
+        User currentUser = getCurrentUser();
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String password = passwordEncoder.encode(user.getPassword());
+
+        currentUser.setFirstname(user.getFirstname());
+        currentUser.setLastname(user.getLastname());
+        currentUser.setEmail(user.getEmail());
+        currentUser.setPassword(password);
+        userRepository.save(currentUser);
+        logger.debug("Update user infos");
+        //executeInsideTransaction(entityManager -> entityManager.merge(user));
+    }
+
+    @Override
+    public void deleteUser(User user) throws InternalAuthenticationServiceException {
+        userRepository.delete(user);
+        //executeInsideTransaction(entityManager -> entityManager.remove(user));
     }
 
 
@@ -68,5 +117,19 @@ public class UserService implements IUserService{
         }
         User currentUser = findByEmail(currentUserEmail).get();
         return currentUser;
+    }
+
+    //----------- Transaction -----------
+    private void executeInsideTransaction(Consumer<EntityManager> action) {
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            action.accept(entityManager);
+            tx.commit();
+        }
+        catch (RuntimeException e) {
+            tx.rollback();
+            throw e;
+        }
     }
 }
