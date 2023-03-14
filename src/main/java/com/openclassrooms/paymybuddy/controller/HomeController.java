e97a9209f3a2debc23c2bb86b0fe62ec4e464e75
4H0 <ggaetan@proton.me>
2023-03-14 11:41:13 +0100

    Clean home controller & add logging

--- src/main/java/com/openclassrooms/paymybuddy/controller/HomeController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/HomeController.java
@@ -2,41 +2,28 @@ package com.openclassrooms.paymybuddy.controller;
 
 import com.openclassrooms.paymybuddy.model.User;
 import com.openclassrooms.paymybuddy.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.security.Principal;
-import java.util.Map;
 
 @Controller
 public class HomeController {
 
     @Autowired
     private IUserService iUserService;
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("/home")
     public String welcomePage(Model model) {
         User currentUser = iUserService.getCurrentUser();
         String welcomMessage = "Welcom " + currentUser.getFirstname() + " " + currentUser.getLastname();
+        logger.debug("{} {} connect the application with his id & password", currentUser.getFirstname(), currentUser.getLastname());
 
         model.addAttribute("currentUser", currentUser);
         model.addAttribute("welcomMessage", welcomMessage);
         return "home";
     }
-
-
-    @GetMapping("/homeOauth2")
-    public String oauthConnexion(Model model){
-        return "homeOauth2";
-    }
-
 }
