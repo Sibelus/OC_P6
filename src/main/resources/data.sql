fe082cb42376cd9020d17077c48dbfcacc17c23c
4H0 <ggaetan@proton.me>
2023-03-17 11:38:12 +0100

    Insert new user & bankAccount for tests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -79,13 +79,15 @@ INSERT INTO `user` (`id`, `email`, `password`, `firstname`, `lastname`, `amount`
 (1, 'mtampion@mail.fr', '$2a$12$GKclbJOO27.E9nFjSE6lJuARu2OFK.0rOYyBABl16t99iWSBzxSPO', 'Mocktar', 'Tampion', 0),
 (2, 'bafritte@mail.fr', '$2a$12$GKclbJOO27.E9nFjSE6lJuARu2OFK.0rOYyBABl16t99iWSBzxSPO', 'Barak', 'Afritte', 0),
 (3, 'dzou@mail.fr', '$2a$12$GKclbJOO27.E9nFjSE6lJuARu2OFK.0rOYyBABl16t99iWSBzxSPO', 'Debby', 'Zou', 0),
-(4, 'jtamar@mail.fr', '$2a$12$GKclbJOO27.E9nFjSE6lJuARu2OFK.0rOYyBABl16t99iWSBzxSPO', 'Justin', 'Tamar', 0);
+(4, 'jtamar@mail.fr', '$2a$12$GKclbJOO27.E9nFjSE6lJuARu2OFK.0rOYyBABl16t99iWSBzxSPO', 'Justin', 'Tamar', 100),
+(5, 'sbihaille@mail.fr', '$2a$12$GKclbJOO27.E9nFjSE6lJuARu2OFK.0rOYyBABl16t99iWSBzxSPO', 'Steff', 'Bihaille', 1000000);
 
 INSERT INTO `bank_account` (`id`, `user_id`, `name`, `amount`) VALUES
 (1, 1, '"Caisse d\'épargne"', 1300),
 (2, 2, 'Boursorama', 1200),
 (3, 3, 'Crédit mutuel', 1500),
-(4, 4, 'Société générale', 1100);
+(4, 4, 'Société générale', 1100),
+(5, 5, 'BCE', 1000000);
 
 INSERT INTO `connection` (`id`, `user_id`, `friend_id`) VALUES
 (1, 1, 2),
